9087fed126c80f61384e2ca0664d8cf01ab1b2b4
aleksov <aleksov101@gmail.com>
2020-12-05 00:19:51 +0200

    add product page and router

--- front_end/src/components/Header.jsx
+++ front_end/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, Nav} from 'react-bootstrap'
 import { FaShoppingCart, FaUserAlt } from 'react-icons/fa';
 
@@ -6,15 +7,27 @@ const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-      <Container>
-        <Navbar.Brand href="/">e-shop</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="/cart"><FaShoppingCart className="mr-1 mb-1"/>Cart</Nav.Link>
-            <Nav.Link href="/login"><FaUserAlt className="mr-1 mb-1"/>Sign In</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>e-shop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart className="mr-1 mb-1" />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUserAlt className="mr-1 mb-1" />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
