b7ca5d6dcd6c029eaa6970de7b012987be323fd9
aleksov <aleksov101@gmail.com>
2020-12-06 17:38:06 +0200

    fetch products from DB

--- back_end/server.js
+++ back_end/server.js
@@ -2,26 +2,22 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
+// Dotenv 
 dotenv.config()
+// Mongo connection
 connectDB()
-
+// Express
 const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+// Routes
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
-
+// Start server
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT, console.log(`Server running on port ${PORT} / ${process.env.NODE_ENV} mode`.yellow.bold.underline))
