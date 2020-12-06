197664aebff88c135a5d82867a6414b118269a40
aleksov <aleksov101@gmail.com>
2020-12-06 23:29:26 +0200

    add erros handler middleware

--- back_end/server.js
+++ back_end/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 // Dotenv 
@@ -15,8 +16,12 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
+// MIDDLEWARE
 // Routes
 app.use('/api/products', productRoutes)
+// Error handlers
+app.use(notFound)
+app.use(errorHandler)
 
 // Start server
 const PORT = process.env.PORT || 5000
