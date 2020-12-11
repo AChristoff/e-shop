bd624b202b4729eb6c400711c704637f4e76e886
aleksov <aleksov101@gmail.com>
2020-12-12 00:21:03 +0200

    updatefooter logo

--- front_end/src/components/Footer.jsx
+++ front_end/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <Container>
       <Row>
         <Col className="text-center py-3">
-          Copyright &copy; e-shop
+          Copyright &copy; e-Shop
         </Col>
       </Row>
     </Container>
