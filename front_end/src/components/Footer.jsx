9782120016e446ae6d1c3c1f4fd7127ae0724f13
aleksov <aleksov101@gmail.com>
2020-12-12 00:50:39 +0200

    update footer content

--- front_end/src/components/Footer.jsx
+++ front_end/src/components/Footer.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import { FaRegCopyright } from 'react-icons/fa'
+import { HiCursorClick } from 'react-icons/hi';
+import logo from '../assets/aleksov-logo.svg';
+
+const currentYear = new Date().getFullYear();
 
 const Footer = () => {
   return (
     <footer style={{'margin-top': 'auto'}}>
-    <Container>
+    <Container className='border-top'>
       <Row>
-        <Col className="text-center py-3">
-          Copyright &copy; e-Shop
+        <Col className="text-center py-2">
+          <FaRegCopyright className="copy-icon"/> 2019-{currentYear} All rights reserved
+          <strong style={{'margin': '0 0 0 0.5em', 'font-weight': '700', 'font-size': '1.2em'}}><HiCursorClick /> e-Shop</strong> | developed by
+          <a className="developer" href="https://alekshristov.com/" target="_blank">
+            <img title="aleksov-logo" src={logo} style={{'height': '1.55em', 'margin': '-0.2em 0 0 0.4em', 'opacity': '0.9'}}></img>
+          </a>
         </Col>
       </Row>
     </Container>
