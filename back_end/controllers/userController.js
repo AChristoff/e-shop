32dbf086846705d69dec0d66ce8cd5cf8a679ae9
aleksov <aleksov101@gmail.com>
2020-12-15 22:45:08 +0200

    add user edit

--- back_end/controllers/userController.js
+++ back_end/controllers/userController.js
@@ -10,7 +10,7 @@ const authUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email })
 
-  if(user && (await user.matchPassword(password))) {
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
@@ -40,10 +40,10 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     name,
     email,
-    password
+    password,
   })
 
-  if(user) {
+  if (user) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -61,20 +61,49 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route:    GET /api/users/profile
 // @access:   Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id) 
+  const user = await User.findById(req.user._id)
 
-  if(user) {
+  if (user) {
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
     })
-  }else {
+  } else {
     res.status(404)
     throw new Error('User not found')
   }
 })
 
+// @desc:     Update user profile
+// @route:    PUT /api/users/profile
+// @access:   Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
 
-export { authUser, getUserProfile, registerUser }
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    // Password is encrypted by the model on change so update it only if there is a new one
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updateUser = await user.save()
+
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+      token: generateToken(updateUser._id),
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile }
