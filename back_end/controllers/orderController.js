77fbfbbc6305679773dfcc70bfc240149126834b
aleksov <aleksov101@gmail.com>
2020-12-27 11:17:00 +0200

    get order by id route

--- back_end/controllers/orderController.js
+++ back_end/controllers/orderController.js
@@ -15,7 +15,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
     totalPrice,
   } = req.body
 
-  if(orderItems && orderItems.length === 0) {
+  if (orderItems && orderItems.length === 0) {
     res.status(400)
     throw new Error('No order items')
     return
@@ -37,4 +37,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems }
+// @desc:     Get order by ID
+// @route:    GET /api/order/:id
+// @access:   Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if(order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById }
--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,34 +1,49 @@
 import React from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
+
+import Loader from './components/Loader'
 import Header from './components/Header'
-import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
-import LoginScreen from './screens/LoginScreen'
-import RegisterScreen from './screens/RegisterScreen'
-import ProfileScreen from './screens/ProfileScreen'
-import ShippingScreen from './screens/ShippingScreen'
+import Footer from './components/Footer'
+// import ProductScreen from './screens/ProductScreen'
+// import CartScreen from './screens/CartScreen'
+// import LoginScreen from './screens/LoginScreen'
+// import RegisterScreen from './screens/RegisterScreen'
+// import ProfileScreen from './screens/ProfileScreen'
+// import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 
+const ProductScreen = React.lazy(() => import('./screens/ProductScreen'))
+const CartScreen = React.lazy(() => import('./screens/CartScreen'))
+const LoginScreen = React.lazy(() => import('./screens/LoginScreen')) 
+const RegisterScreen = React.lazy(() => import('./screens/RegisterScreen'))
+const ProfileScreen = React.lazy(() => import('./screens/ProfileScreen'))
+const ShippingScreen = React.lazy(() => import('./screens/ShippingScreen'))
+// const PaymentScreen = React.lazy(() => import('./screens/PaymentScreen'))
+// const PlaceOrderScreen = React.lazy(() => import('./screens/PlaceOrderScreen'))
+
 const App = () => {
-  return ( 
+  return (
     <Router>
       <Header />
-      <main className="py-3">
-      <Container>
-        <Route path='/shipping' component={ShippingScreen} />
-        <Route path='/payment' component={PaymentScreen} />
-        <Route path='/placeorder' component={PlaceOrderScreen} />
-        <Route path='/login' component={LoginScreen} />
-        <Route path='/register' component={RegisterScreen} />
-        <Route path='/profile' component={ProfileScreen} />
-        <Route path='/product/:id' component={ProductScreen} />
-        <Route path='/cart/:id?' component={CartScreen} />
-        <Route path='/' component={HomeScreen} exact/>
-      </Container>
+      <main className='py-3'>
+        <React.Suspense fallback={<Loader />}>
+          <Switch>
+            <Container>
+              <Route path='/shipping' component={ShippingScreen} />
+              <Route path='/payment' component={PaymentScreen} />
+              <Route path='/placeorder' component={PlaceOrderScreen} />
+              <Route path='/login' component={LoginScreen} />
+              <Route path='/register' component={RegisterScreen} />
+              <Route path='/profile' component={ProfileScreen} />
+              <Route path='/product/:id' component={ProductScreen} />
+              <Route path='/cart/:id?' component={CartScreen} />
+              <Route path='/' component={HomeScreen} exact />
+            </Container>
+          </Switch>
+        </React.Suspense>
       </main>
       <Footer />
     </Router>
