c1164db4583cc4d0e41408f9e8ad09e202db5ebe
aleksov <aleksov101@gmail.com>
2021-01-16 08:19:21 +0200

    change seeders

--- back_end/data/users.js
+++ back_end/data/users.js
@@ -4,18 +4,18 @@ const users = [
   {
     name: 'Admin',
     email: 'admin@example.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync(process.env.TEST_USERS_PASS, 10),
     isAdmin: true,
   },
   {
     name: 'John Doe',
     email: 'john@example.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync(process.env.TEST_USERS_PASS, 10),
   },
   { 
     name: 'Jane Doe',
     email: 'jane@example.com',
-    password: bcrypt.hashSync('123456', 10),
+    password: bcrypt.hashSync(process.env.TEST_USERS_PASS, 10),
   },
 ]
 
